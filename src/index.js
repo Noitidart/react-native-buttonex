fd54a6acf6cfc3516d06ff36e82303bfc6fbc5d8
Noitidart <noitidart@gmail.com>
2017-12-09 15:13:18 -0800

    trying first publish

---
+++ src/index.js
@@ -0,0 +1,113 @@
+// @flow
+
+import React, { Component } from 'react'
+import { Animated, Platform, Text, TouchableNativeFeedback, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native'
+
+import styles from './styles'
+
+type Props = {
+    accessibilityLabel?: string, // Text to display for blindness accessibility features
+    bold?: boolean, // *(iOS only)* always bold on android, but on iOS default is not bold
+    bordered?: boolean,
+    // bordered === false: Color of the text (iOS), or background color of the button (Android)
+    // bordered === true: Color of the text and border and background on press (iOS), or color of the text and border of the button (Android)
+    color?: string,
+    disabled?: boolean,
+    hasTVPreferredFocus?: boolean, // *(Apple TV only)* TV preferred focus (see documentation for the View component).
+    noShadow?: boolean, // *(Android only)* by default it is elevated, if bordered OR disabled are true, then this has no effect as both of these force no shadow
+    onPress: () => any,
+    testID?: string, // Used to locate this view in end-to-end tests.
+    title: string, // Text to display for blindness accessibility features
+    transparent?: boolean, // *(Android only)* iOS is always transparent. blocks/superceeds "bordered"
+}
+
+type State = {
+    activeAnim: typeof Animated.Value
+}
+
+class Button extends Component<Props, State> {
+    state = {
+        activeAnim: new Animated.Value(0)
+    }
+
+    render() {
+        const { accessibilityLabel, bold, bordered, color, disabled, hasTVPreferredFocus, noShadow, onPress, testID, title, transparent } = this.props;
+        const { activeAnim } = this.state;
+
+        const isIOS = Platform.OS === 'ios';
+        const isAndroid = !isIOS;
+
+        const buttonStyles = [styles.button];
+        const textStyles = [styles.text];
+        const accessibilityTraits = ['button'];
+
+        if (disabled) {
+            accessibilityTraits.push('disabled');
+            buttonStyles.push(styles.buttonDisabled);
+            textStyles.push(styles.textDisabled);
+            if (bordered) buttonStyles.push(styles.buttonBordered, styles.buttonBorderedDisabled);
+            if (isAndroid) {
+                if (transparent) buttonStyles.push(styles.buttonTransparentDisabledAndroid);
+            }
+        } else {
+            if (transparent && isAndroid) {
+                buttonStyles.push(styles.buttonTransparentAndroid);
+            }
+            if (bordered && !(isAndroid && transparent)) {
+                buttonStyles.push(styles.buttonBordered);
+                if (isIOS) textStyles.push(styles.textBordered); // dont push if isAndroid because textBordered will give it color
+            }
+            if (color) {
+                if (bordered) buttonStyles.push({ borderColor:color });
+                if (isIOS || (isAndroid && (transparent || bordered))) textStyles.push({ color });
+                else if (!transparent) buttonStyles.push({ backgroundColor:color }); // isAndroid on this line
+            } else {
+                if (isAndroid && (transparent || bordered)) textStyles.push(styles.textBordered); // textBordered only has color on android
+            }
+            if (noShadow && isAndroid && !bordered && !transparent) {
+                buttonStyles.push(styles.buttonUnelevatedAndroid);
+            }
+        }
+
+        if (bold && isIOS) {
+            textStyles.push(styles.textBold);
+        }
+
+        const formattedTitle = isAndroid ? title.toUpperCase() : title;
+        const Touchable = isAndroid ? TouchableNativeFeedback : TouchableOpacity;
+
+        if (isIOS && bordered) {
+            return (
+                <View style={buttonStyles}>
+                    <Text style={textStyles} disabled={disabled}>{formattedTitle}</Text>
+                    <TouchableWithoutFeedback accessibilityComponentType="button" accessibilityLabel={accessibilityLabel} accessibilityTraits={accessibilityTraits} disabled={disabled} hasTVPreferredFocus={hasTVPreferredFocus} onPress={onPress} onPressIn={this.activateIOS} onPressOut={this.deactivateIOS} testID={testID}>
+                        <Animated.View style={[{opacity:activeAnim}, styles.buttonBorderedActiveIOS, color && { backgroundColor:color }]}>
+                            <Text style={[styles.text, styles.textBorderedActiveIOS, bold && styles.textBold]}>{formattedTitle}</Text>
+                        </Animated.View>
+                    </TouchableWithoutFeedback>
+                </View>
+            )
+        } else {
+            return (
+                <Touchable accessibilityComponentType="button" accessibilityLabel={accessibilityLabel} accessibilityTraits={accessibilityTraits} disabled={disabled} hasTVPreferredFocus={hasTVPreferredFocus} onPress={onPress} testID={testID}>
+                    <View style={buttonStyles}>
+                        <Text style={textStyles} disabled={disabled}>{formattedTitle}</Text>
+                    </View>
+                </Touchable>
+            )
+        }
+    }
+
+    activateIOS = () => {
+        const { activeAnim } = this.state;
+        activeAnim.stopAnimation();
+        Animated.timing(activeAnim, { toValue:1, duration:200, useNativeDriver:true }).start();
+    }
+    deactivateIOS = () => {
+        const { activeAnim } = this.state;
+        activeAnim.stopAnimation();
+        Animated.timing(activeAnim, { toValue:0, duration:200, useNativeDriver:true }).start();
+    }
+}
+
+export default Button
---
+++ src/styles.js
@@ -0,0 +1,105 @@
+// @flow
+
+import { Platform, StyleSheet } from 'react-native'
+
+// ios content blue - #0080FC
+// ios header blue - #007AFF also the iOS blue from https://developer.apple.com/ios/human-interface-guidelines/visual-design/color/
+
+const styles = StyleSheet.create({
+    button: Platform.select({
+        ios: undefined,
+        android: {
+            elevation: 4,
+            backgroundColor: '#2196F3', // Material design blue from https://material.google.com/style/color.html#color-color-palette
+            borderRadius: 2
+        }
+    }),
+    buttonTransparentAndroid: {
+        backgroundColor: 'transparent',
+        elevation: 0
+    },
+    buttonBordered: Platform.select({
+        ios: {
+            borderWidth: 1,
+            borderColor: '#007AFF',
+            borderRadius: 6,
+            overflow: 'hidden' // otherwse backgroundColor messes up borderRadius
+        },
+        android: {
+            elevation: 0,
+            borderColor: '#2196F3',
+            borderWidth: 1,
+            backgroundColor: '#FFFFFF'
+        }
+    }),
+    buttonUnelevatedAndroid: {
+        elevation: 0
+    },
+    text: Platform.select({
+        ios: {
+            color: '#007AFF', // iOS blue from https://developer.apple.com/ios/human-interface-guidelines/visual-design/color/
+            textAlign: 'center',
+            padding: 8,
+            fontSize: 18
+        },
+        android: {
+            color: '#FFFFFF',
+            textAlign: 'center',
+            padding: 8,
+            fontWeight: '500'
+        }
+    }),
+    textBold:Platform.select({
+        ios: {
+            fontWeight: '500'
+        },
+        android: undefined
+    }),
+    textBordered: Platform.select({
+        ios: {
+            paddingVertical: 12 // link3918
+        },
+        android: {
+            color: '#2196F3'
+        }
+    }),
+    buttonDisabled: Platform.select({
+        ios: undefined,
+        android: {
+            elevation: 0,
+            backgroundColor: '#DFDFDF'
+        }
+    }),
+    buttonBorderedDisabled: Platform.select({
+        ios: {
+            borderColor: '#CDCDCD'
+        },
+        android: {
+            borderColor: '#DFDFDF',
+            backgroundColor: '#FFFFFF'
+        }
+    }),
+    buttonTransparentDisabledAndroid: {
+        backgroundColor: 'transparent'
+    },
+    textDisabled: Platform.select({
+        ios: {
+            color: '#CDCDCD'
+        },
+        android: {
+            color: '#A1A1A1'
+        }
+    }),
+    textBorderedActiveIOS: {
+        paddingVertical: 12, // link3918
+        color: '#FFFFFF'
+    },
+    buttonBorderedActiveIOS: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#007AFF'
+    }
+})
+
+export default styles
